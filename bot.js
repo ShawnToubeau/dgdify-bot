91c148ab0883441b6c0d47d1cc20c2e1d7bb2af0
Shawn Toubeau <shawntoubeau@gmail.com>
2018-07-01 22:36:46 -0400

    Testing

--- bot.js
+++ bot.js
@@ -2,8 +2,17 @@ var Twit = require('twit');
 var TwitterBot = require('node-twitterbot').TwitterBot;
 const markov = require('markov');
 var fs = require('fs');
-var moment = require('moment');
+var moment = require('moment-timezone');
+const config = require('./config')
 
+module.exports = {
+  twitterKeys: {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  }
+}
 
 var Bot = new TwitterBot({
  consumer_key: process.env.BOT_CONSUMER_KEY,
@@ -11,6 +20,8 @@ var Bot = new TwitterBot({
  access_token: process.env.BOT_ACCESS_TOKEN,
  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
+
+// const Bot = new Twit(config.twitterKeys)
 
 var chain = markov(3);
 
@@ -22,13 +33,20 @@ if (hour >= 6 && hour <= 18) {
     fs.readFile('tilian.txt', 'utf8', function(err, data) {  
     if (err) throw err;
         lyrics = data;
-        Bot.tweet(lyrics);
+        // Bot.tweet(lyrics);
+        Bot.post('statuses/update', { status: lyrics }, function(err, data, response) {
+            console.log(data)
+        })
     });
 } else {
     //jon
     fs.readFile('jon.txt', 'utf8', function(err, data) {  
     if (err) throw err;
         lyrics = data;
-        Bot.tweet(lyrics);
+        // Bot.tweet(lyrics);
+        Bot.post('statuses/update', { status: lyrics }, function(err, data, response) {
+            console.log(data)
+        })
     });
 }
+
