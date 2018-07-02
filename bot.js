1205e4dd538dbb66cbd1e2d5b124dafb86443329
Shawn Toubeau <shawntoubeau@gmail.com>
2018-07-02 19:02:16 -0400

    Bot is functioning

--- bot.js
+++ bot.js
@@ -14,39 +14,72 @@ module.exports = {
   }
 }
 
-var Bot = new TwitterBot({
- consumer_key: process.env.BOT_CONSUMER_KEY,
- consumer_secret: process.env.BOT_CONSUMER_SECRET,
- access_token: process.env.BOT_ACCESS_TOKEN,
- access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
+// var Bot = new TwitterBot({
+//  consumer_key: process.env.BOT_CONSUMER_KEY,
+//  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+//  access_token: process.env.BOT_ACCESS_TOKEN,
+//  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+// });
 
-// const Bot = new TwitterBot(config.twitterKey)
+var Bot = new TwitterBot({
+ consumer_key: 'Qf2gQMuWfCST1b2ZKwKnr7iPF',
+ consumer_secret: '5sq6lTRv1QbxKLjfFyXWhQZ9FhcfouTsbKt7aA12sDMCgjqTvQ',
+ access_token: '999879340941807617-qO86BVKAci6osy4XfIt9WcWFRaSvVbF',
+ access_token_secret: 'UJq9IbvWS8yFUWBaJD29WDgWeLcHlMADtwqs60jdaeHdq'
+});
 
 var chain = markov(3);
 
 var hour = moment().tz("America/New_York").format("H")
-var lyrics = '';
+var lyrics = null;
 
-if (hour >= 6 && hour <= 18) {
-    // tilian
-    fs.readFile('tilian.txt', 'utf8', function(err, data) {  
-    if (err) throw err;
-        lyrics = data;
-        Bot.tweet(lyrics);
-        // Bot.post('statuses/update', { status: lyrics }, function(err, data, response) {
-        //     console.log(data)
-        // })
-    });
-} else {
-    //jon
-    fs.readFile('jon.txt', 'utf8', function(err, data) {  
-    if (err) throw err;
-        lyrics = data;
-        Bot.tweet(lyrics);
-        // Bot.post('statuses/update', { status: lyrics }, function(err, data, response) {
-        //     console.log(data)
-        // })
-    });
+const tweet = () => {
+    console.log("tweet")
+    console.log("setting lyrics..")
+    if (hour >= 6 && hour <= 18) {
+        //console.log('tilian')
+        // tilian
+        fs.readFile('tilian.txt', 'utf8', function(err, data) {  
+            if (err) throw err;
+                lyrics = data;
+                console.log("set lyrics to tilian")
+                chain.seed(lyrics, function() {
+                    console.log("making tweet")
+                    try {  
+                        var res = chain.respond('<Buffer 0a>', 8).join(' ');
+                        Bot.tweet(res);
+                        console.log(res)
+                        // Bot.post('statuses/update', { status: lyrics }, function(err, data, response) {
+                        //     console.log(data)
+                        // })
+                    } catch(e) {
+                        console.log('Error:', e.stack);
+                    }
+                }); 
+            });
+    } else {
+        // console.log('jon')
+        //jon
+        fs.readFile('jon.txt', 'utf8', function(err, data) {  
+        if (err) throw err;
+            lyrics = data;
+            console.log("set lyrics to jon")
+            chain.seed(lyrics, function() {
+                console.log("making tweet")
+                try {  
+                    var res = chain.respond('<Buffer 0a>', 8).join(' ');
+                    Bot.tweet(res);
+                    console.log(res)
+                    // Bot.post('statuses/update', { status: lyrics }, function(err, data, response) {
+                    //     console.log(data)
+                    // })
+                } catch(e) {
+                    console.log('Error:', e.stack);
+                }
+            }); 
+        });
+    }
 }
+
+tweet();
 
